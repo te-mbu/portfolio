b0ec8c1f6e7d05fe8d6f89c65dca6c5c1f1f5a6a
Terence Mbu <mbu.terence@gmail.com>
2023-07-08 23:51:39 +0200

    fix transition

--- components/TransitionEffect.js
+++ components/TransitionEffect.js
@@ -4,14 +4,14 @@ import { motion } from 'framer-motion'
 const TransitionEffect = () => {
   return (
     <>
-    <motion.div className='fixed top-0 bottom-0 right-full w-full h-full z-30 bg-primary'
+    <motion.div className='fixed top-0 bottom-0 right-full w-full h-full z-30 bg-light'
     initial={{x: '100%', width:'100%'}}
     animate={{x: '0%', width:'0%'}}
     exit={{x:["0%", "100%"], width:["0%", "100%"]}}
     transition={{duration:0.8, ease:"easeInOut"}}
     />
 
-<motion.div className='fixed top-0 bottom-0 right-full w-full h-full z-20 bg-light'
+<motion.div className='fixed top-0 bottom-0 right-full w-full h-full z-20 bg-primary'
     initial={{x: '100%', width:'100%'}}
     animate={{x: '0%', width:'0%'}}
     transition={{delay:0.2, duration:0.8, ease:"easeInOut"}}
