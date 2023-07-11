54ff479ee896fc58facb89923de93c8f0037f579
Terence Mbu <mbu.terence@gmail.com>
2023-07-11 09:33:23 +0200

    app js overflow

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,7 @@ function App({ Component, pageProps }) {
         <link rel="icon" href="#" /> /
       </Head>
       <main
-        className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen overflow-auto`}
+        className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
       >
         <NavBar />
         <AnimatePresence mode="wait">
