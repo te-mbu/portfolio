3c842ca3267b839c83be29d59f53302d9f36a1c3
Terence Mbu <mbu.terence@gmail.com>
2023-07-11 10:13:56 +0200

    change bg colors animation

--- components/TransitionEffect.js
+++ components/TransitionEffect.js
@@ -4,8 +4,8 @@ import { motion } from "framer-motion";
 const TransitionEffect = () => {
   return (
     <>
-      <motion.div
-        className="fixed top-0 bottom-0 right-full w-full h-full z-30 bg-primary"
+     <motion.div
+        className="fixed top-0 bottom-0 right-full w-full h-full z-30 bg-light dark:bg-dark"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "0%" }}
         exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
@@ -13,14 +13,14 @@ const TransitionEffect = () => {
       />
 
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-full h-full z-20 bg-light"
+        className="fixed top-0 bottom-0 right-full w-full h-full z-20 bg-dark dark:bg-primaryDark"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "0%" }}
         transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
       />
 
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-full h-full z-10 bg-dark"
+        className="fixed top-0 bottom-0 right-full w-full h-full z-10 bg-primary dark:bg-light"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "0%" }}
         transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
